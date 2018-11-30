4adc79c8480a9199e46fe4611be0ce21ec412b5e
adikantawala <adikan@ucla.edu>
2018-11-30 15:38:49 -0800

    Add working clock w date/time and style

---
+++ widgets/frontend/clock.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+class Clock extends React.Component {
+  
+  constructor(){
+    super();
+    let time = new Date();
+    this.tick = this.tick.bind(this);
+    this.state = {time};
+  }
+  
+  componentDidMount() {
+    setInterval(this.tick, 1000);
+  }
+  
+  tick() {
+    this.setState({ time: new Date() });
+  }
+  
+  render() {
+    return (
+      <div className="clock"> 
+        <h1>Time: { this.state.time.toLocaleTimeString('en-US') }</h1> 
+        <h1>Date: { this.state.time.toDateString() }</h1>
+      </div>
+    );
+  } //look for a method to help parse time
+  
+}
+
+export default Clock;
